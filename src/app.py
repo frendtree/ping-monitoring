f48af62b1f8bde401f01c71f50a7d7453133d6ca
frendtree <38940431+frendtree@users.noreply.github.com>
2021-10-17 15:17:03 +0900

    add: 成功時にメールを送信
    refs: #1

--- src/app.py
+++ src/app.py
@@ -17,15 +17,24 @@ mail_to = os.environ['MAIL_TO']
 mail_cc = os.environ.get('MAIL_CC', '')
 
 
-def send_mail():
+def send_mail_when_succeeded():
+    subject = '[OK] PING succeeded ' + ping_host
+    body = 'PING to {0} succeeded at {1:%Y-%m-%d %H:%M:%S}'.format(ping_host, get_datetime_now())
+    send_mail(subject, body)
+
+
+def send_mail_when_failed():
     subject = '[CRITICAL] PING failed ' + ping_host
     body = 'PING to {0} failed at {1:%Y-%m-%d %H:%M:%S}'.format(ping_host, get_datetime_now())
+    send_mail(subject, body)
+
+
+def send_mail(subject, body):
     msg = MIMEText(body, 'html')
     msg['Subject'] = subject
     msg['From'] = mail_from
     msg['To'] = mail_to
     msg['Cc'] = mail_cc
-
     server = smtplib.SMTP(mail_smtp_host, mail_smtp_port)
     server.starttls()
     server.login(mail_account, mail_password)
@@ -43,9 +52,10 @@ def main():
     response = ping.ping(ping_host, 2)
     if response.is_reached():
         print('Success!\n')
+        send_mail_when_succeeded()
     else:
         print('Failed!\n')
-        send_mail()
+        send_mail_when_failed()
     exit()
 
 
